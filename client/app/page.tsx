568e833499a0fefd31918358a62df381b1268a5c
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-05-05 11:38:47 +0530

    client talkin to server successfully

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 
 const LandingPage = ()=>{
   const handleSeverHealthCheck = async ()=>{
-    const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL as string)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL as string}/health`,{
+      method:'GET'
+    })
     const data = await response.json()
     console.log("backend response: ", data) 
   }
--- server/middleware/cors/corsOrigin.js
+++ server/middleware/cors/corsOrigin.js
@@ -5,11 +5,9 @@ import cors from 'cors'
  * @param {import("express").Response} res 
  * @param {import("express").NextFunction} next 
  */
-export const corsFunction = (req, res, next)=>{
-    cors({
-        credentials:true,
-        allowedHeaders:["Authorization", "x-aws-clone-auth"],
-        methods:["POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"],
-        origin:["http://localhost:3000"],
-    })
-}
+export const corsFunction = cors({
+    credentials: true,
+    allowedHeaders: ["Authorization", "x-aws-clone-auth"],
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    origin: ["http://localhost:3000"],
+})
